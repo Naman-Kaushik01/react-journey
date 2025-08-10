2388ab8109d192cd40b13e125bfde7fcc9be0127
Naman Kaushik <kaushiknaman68@gmail.com>
2025-08-10 20:55:42 +0530

    state-class

---
+++ react-state-class/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Counter from './Counter'
+import LikeButton from './LikeButton'
+function App() {
+  
+
+  return (
+    <div>
+      <h1>States in React</h1>
+      {/* <Counter/> */}
+      <LikeButton/>
+    </div>
+    
+  )
+}
+
+export default App
