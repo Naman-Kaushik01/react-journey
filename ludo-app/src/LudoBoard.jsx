4321955685c4b1311b490bd0fcffe6aadbbf0cf1
Naman Kaushik <kaushiknaman68@gmail.com>
2025-08-13 09:31:15 +0530

    ludo

---
+++ ludo-app/src/LudoBoard.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+export default function LudoBoard(){
+    let [moves,setMoves]=useState({blue :0,red:0,yellow:0,green:0});
+
+    let updateBlue =()=>{
+        console.log(`Moves=${moves}`);
+        setMoves((preMoves)=>{
+            return{...preMoves, blue: preMoves.blue +1};
+        });
+    }
+     let updateYellow =()=>{
+        console.log(`Moves=${moves}`);
+        setMoves((preMoves)=>{
+            return{...preMoves, yellow: preMoves.yellow +1};
+        });
+    }
+     let updateGreen =()=>{
+        console.log(`Moves=${moves}`);
+        setMoves((preMoves)=>{
+            return{...preMoves, green: preMoves.green +1};
+        });
+    }
+     let updateRed =()=>{
+        console.log(`Moves=${moves}`);
+        setMoves((preMoves)=>{
+            return{...preMoves, red: preMoves.red +1};
+        });
+    }
+    return (
+        <div>
+            <p>Game Begins</p>
+            <div className="board">
+                <p >Blue Moves = {moves.blue}</p>
+                <button style={{backgroundColor:"blue"}} onClick={updateBlue}>+1</button>
+                <p>Yellow Moves ={moves.yellow}</p>
+                <button style= {{backgroundColor:"yellow"}} onClick={updateYellow}>+1</button>
+                <p>Green Moves ={moves.green}</p>
+                <button style={{backgroundColor:"green"}} onClick={updateGreen}>+1</button>
+                <p>Red Moves ={moves.red}</p>
+                <button style={{backgroundColor:"red"}} onClick={updateRed}>+1</button>
+            </div>
+        </div>
+    )
+}
